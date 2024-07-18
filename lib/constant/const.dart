12e1dcea8799e2db897eb674711ef7f22e28d74e
AQUIB <aquib.saudagar@cwdin.com>
2024-07-18 17:47:48 +0530

    introduction to cubit

---
+++ lib/constant/const.dart
@@ -0,0 +1,13 @@
+class Constants {
+  static const name = [
+    'Aquib',
+    'Mukesh',
+    'Viddyut',
+    'Ankush',
+    'Arbaz',
+    'Rahul',
+    'Ganesh',
+    'Sagar',
+    'Vedhant'
+  ];
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:bloc_refresh/cubits/cubits.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late final NameCubits cubits;
+  @override
+  void initState() {
+    cubits = NameCubits();
+    automizer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    cubits.close();
+    super.dispose();
+  }
+
+  void automizer() {
+    Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      if (timer.isActive) {
+        cubits.pickRandomName();
+        if (timer.tick == 50) {
+          timer.cancel();
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bloc  Refresh"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: StreamBuilder<String?>(
+            stream: cubits.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(
+                  "${snapshot.data}",
+                  style: const TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey),
+                );
+              } else {
+                return Text("${snapshot.connectionState}");
+              }
+            }),
+      ),
+    );
+  }
+}
